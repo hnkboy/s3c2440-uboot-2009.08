4b21fbfb8afed4b364a981060de5e08c322c49b8
hnkboy <709256704@qq.com>
2017-04-08 15:33:33 +0800

    头文件的修改，和cpu和ram初始化的屏蔽要注意

--- include/configs/smdk2440.h
+++ include/configs/smdk2440.h
@@ -35,8 +35,8 @@
  */
 #define CONFIG_ARM920T		1	/* This is an ARM920T Core	*/
 #define	CONFIG_S3C2410		1	/* in a SAMSUNG S3C2410 SoC     */
-#define CONFIG_SMDK2410		1	/* on a SAMSUNG SMDK2410 Board  */
-#define CONFIG_SMDK2440     1
+#define CONFIG_SMDK2410         1
+#define CONFIG_S3C2440          1
 /* input clock of PLL */
 #define CONFIG_SYS_CLK_FREQ	12000000/* the SMDK2410 has 12MHz input clock */
 
